5ec7a9abca01bab454b9b011d6ef1369070cd2f7
hipema <a19hipema@iesgrancapitan.org>
2020-02-03 00:19:18 +0100

    Version 1.0

---
+++ poo/Circulo.java
@@ -0,0 +1,52 @@
+package poo;
+import javax.swing.JOptionPane;
+
+/**
+ * Esta clase crea objetos del tipo Círculo
+ * 
+ * @version 1.0
+ * @author manuelhidalgo
+ *
+ */
+public class Circulo {
+// Atributos
+	private double radio;
+	
+// Constructor
+	Circulo (double radio){
+		this.radio = radio;
+	}		
+	
+// Getters y setters
+	public double getRadio() {
+		return radio;
+	}
+
+	public void setRadio(double radio) {
+		String mensaje = JOptionPane.showInputDialog("Soy un mísero punto sin área");
+		if (radio == 0) {
+			System.out.println(mensaje);
+		}
+		this.radio = radio;
+	}
+
+// Métodos de la clase
+	public double area () {
+		double area = Math.PI * Math.pow(this.getRadio(), 2);
+		return area;
+	}
+	
+	public void crecerCirculo (int valor) {
+		this.radio *= valor;
+	}
+	
+	public void menguarCirculo (int valor) {
+		this.radio /= valor;
+	}
+	
+// Métodos
+	
+	public String toString () {
+		return "Soy un círculo de radio "+ this.radio + " metros. Ocupo un área de " + this.area() + " metros cuadrados.";
+	}
+}

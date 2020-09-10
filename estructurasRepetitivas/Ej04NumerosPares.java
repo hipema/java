dc8c19396d04c1a81d94eae7baa72035f5b96fea
hipema <a19hipema@iesgrancapitan.org>
2020-09-10 23:54:20 +0200

    Actualización ejercicios

--- estructurasRepetitivas/Ej04NumerosPares.java
+++ estructurasRepetitivas/Ej04NumerosPares.java
@@ -1,47 +1,46 @@
 package estructurasRepetitivas;
 
-/*
+/**
 04 Ejercicios Estructuras Repetitivas en Java
 
 Programa:
 Escribir un programa que imprima todos los números pares entre dos números
 que se le pidan al usuario.
 
-Autor: Manuel Hidalgo Pérez
-Fecha: 10-11-19
-
-Variables a utilizar:
-    numero1 (int)
-    numero2 (int)
-    i (int)
-
-ALGORITMO
-    - LEER numero1, numero2
-    - Cálculos
-        Crear rango con la función "for in" y dar condición.
-            Si i%2 == 0 --> implica que es par y se ha de imprimir''
+@author: manolohidalgo_
+@date: 10-11-19
 */
 
 import java.util.Scanner;
 // Inicio del programa y declaración de variables:
 public class Ej04NumerosPares {
   public static void main(String[] args){
-    Scanner s = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
     int numero1;
     int numero2;
-    double i;
+    int minimo;
+    int maximo;
 
-// Solicitud de datos al usuario
+    // Solicitud de datos al usuario
     System.out.println("El programa imprimirá todos los números pares entre dos números indicados.");
     System.out.print("Introduce el primer número: ");
-    numero1 = s.nextInt();
+    numero1 = scanner.nextInt();
     System.out.print("Introduce el segundo número: ");
-    numero2 = s.nextInt();
-// Realización del ciclo para hacer cálculos y mostrar en pantalla
-    for (i=numero1; i <= numero2; i = i+1){
+    numero2 = scanner.nextInt();
+    
+    // Ordenamos los valores en mayor y menor
+    if (numero1 < numero2) {
+      minimo = numero1;
+      maximo = numero2;
+    } else {
+      minimo = numero2;
+      maximo = numero1;
+    }
+    // Realización del ciclo para hacer cálculos y mostrar en pantalla
+    for (int i=minimo+1; i < maximo; i++){
       if (i%2==0){
-        System.out.println((int)i); 
-        }
+        System.out.println(i); 
+      }
     }
   }
 }
--- estructurasRepetitivas/Ej05Limites.java
+++ estructurasRepetitivas/Ej05Limites.java
@@ -1,6 +1,6 @@
 package estructurasRepetitivas;
 
-/*
+/**
 05 Ejercicios Estructuras Repetitivas en Java
 
 Programa:
@@ -14,93 +14,67 @@ Cuando termine el programa dará las siguientes informaciones:
 - Cuantos números están fuera del intervalo.
 - Informa si hemos introducido algún número igual a los límites del intervalo.
 
-Autor: Manuel Hidalgo Pérez
-Fecha: 10-11-19
-
-Variables a utilizar:
-    inferior (int)
-    superior (int)
-    numero (int)
-    suma (int)
-    fueraIntervalo (int)
-    coincidencia (int)
-
-ALGORITMO
-    LEER inferior, superior
-    COMPROBAR inferior < superior
-        Si es cierto continuamos al siguiente paso
-        Si no es cierto, repetir lectura de números
-    LEER numero
-        Si numero != 0
-            si numero >= inferior and numero <= superior --> suma = suma + numero
-            si numero < inferior or numero > superior --> fuera_intervalo +=1
-            si numero == inferior or numero == superior --> coincidencia +=1
-        Si numero == 0
-            Mostrar suma
-            Mostrar fuera_intervalo
-            Mostrar coincidencia
+@author: manolohidalgo_
+@date: 10-11-19
 */
 
 import java.util.Scanner;
 // Inicio del programa y declaración de variables:
 public class Ej05Limites {
   public static void main(String[] args){
-    Scanner s = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
     int inferior;
     int superior;
     int numero;
     int suma = 0;
-    int fueraIntervalo = 0;
-    int coincidencia = 0;
+    int contador = 0;
+    boolean esLimite = false;
 
-// Inicio del programa, explicación al usuario
+    // Inicio del programa, explicación al usuario
     System.out.println("El programa solicitará un límite inferior y superior de intervalo.");
-    System.out.println("Después pedirá la introducción de números a analizar, para finalizar de introducir números, pulsa 0");
+    System.out.println("Después pedirá la introducción de números a analizar, para finalizar "
+        + "de introducir números, pulsa 0");
     System.out.println("Mostrará la siguiente información: ");
     System.out.println("Suma de los números que están dentro del intervalo");
     System.out.println("Cuantos números están fuera del intervalo");
-    System.out.println("Si se ha introducido alguno de los números que marcan los límites de los intervalos");
-// Pedimos límites al usuario
-    System.out.print("Introduce el límite inferior: ");
-    inferior = s.nextInt();
-    System.out.print("Introduce el límite superior: ");
-    superior = s.nextInt();
-// Comprobamos valores de los límites
-    while (inferior > superior){
+    System.out.println("Si se ha introducido alguno de los números que marcan los límites "
+        + "de los intervalos");
+    // Pedimos límites al usuario
+    do {
       System.out.print("Introduce el límite inferior: ");
-      inferior = s.nextInt();
+      inferior = scanner.nextInt();
       System.out.print("Introduce el límite superior: ");
-      superior = s.nextInt();
+      superior = scanner.nextInt();
+      if (inferior > superior) {
+        System.out.println("Error, el límite inferior debe ser que el superior. "
+            + "Revisa los valores introducidos.");
+      }
+    } while (inferior > superior);
+    
+    // Pedimos al usuario insertar números
+    do {
+      System.out.println("Introduce un número: ");
+      numero = scanner.nextInt();
+      
+      if (numero != 0) {
+        if (numero > inferior && numero < superior) {
+          suma += numero;
+        } else if (numero <= inferior || numero >= superior) {
+          contador++;
+        }
+        if (numero == inferior || numero == superior) {
+          esLimite = true;
+        }
+      } 
+    } while (numero != 0);
+    
+    // Al introducir 0, mostramos resultados
+    System.out.println("El valor de la suma de los números introducidos en el rango " + inferior +"-" +superior +" es de " +suma);
+    System.out.println("Se han introducido un total de " + contador + " números fuera del intervalo " + inferior+"-"+superior);
+    if (esLimite){
+      System.out.println("Alguno de los valores introducidos ha coincidido con alguno de los límites del intervalo");
+    } else {
+      System.out.println("No se ha introducido ningún valor igual a los límites del intervalo");
     }
-// Pedimos al usuario insertar números
-    System.out.print("Inserta el número que desees: ");
-    numero = s.nextInt();
-    while (numero != 0){
-        if (numero >= inferior && numero < superior){
-          suma = suma + numero;
-          System.out.print("Inserta un nuevo valor: ");
-          numero = s.nextInt();
-        } else if (numero == superior){
-          System.out.println("Inserta un nuevo valor: ");
-          numero = s.nextInt();
-        } else {
-          fueraIntervalo +=1;
-          System.out.print("Inserta un nuevo valor: ");
-          numero = s.nextInt();
-        }
-        if (numero == inferior || numero == superior){
-          coincidencia +=1;
-          System.out.print("Inserta un nuevo valor: ");
-          numero = s.nextInt();
-        }
-     }
-// Al introducir 0, mostramos resultados
-        System.out.println("El valor de la suma de los números introducidos en el rango " + inferior +"-" +superior +" es de " +suma);
-        System.out.println("Se han introducido un total de " + fueraIntervalo + " números fuera del intervalo " + inferior+"-"+superior);
-        if (coincidencia == 0){
-          System.out.println("No se ha introducido ningún valor igual a los límites del intervalo");
-        } else {
-          System.out.println("Alguno de los valores introducidos ha coincidido con alguno de los límites del intervalo");
-        }
   }
 }

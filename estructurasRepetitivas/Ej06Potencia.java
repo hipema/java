5dc1871fad47f8751a6c091b4349fea6a436faed
hipema <a19hipema@iesgrancapitan.org>
2020-09-13 12:20:49 +0200

    Actualización ejercicios

--- estructurasRepetitivas/Ej06Potencia.java
+++ estructurasRepetitivas/Ej06Potencia.java
@@ -1,14 +1,14 @@
 package estructurasRepetitivas;
 
-/*
+/**
 06 Ejercicios Estructuras Repetitivas en Java
 
 Programa:
 Escribe un programa que dados dos números, uno real (base) y un entero positivo (exponente),
 saque por pantalla el resultado de la potencia. No se puede utilizar el operador de potencia.
 
-Autor: Manuel Hidalgo Pérez
-Fecha: 11-11-19
+@author: manolohidalgo_
+@date: 11-11-19
 
 Variables a utilizar:
     base (double)
@@ -27,27 +27,30 @@ import java.util.Scanner;
 // Inicio del programa y declaración de variables:
 public class Ej06Potencia {
   public static void main(String[] args){
-    Scanner s = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
     double base;
     int exponente;
-    int i;
     double resultado;
 
-// Inicio del programa, explicación al usuario
-    System.out.println("El programa solicitará un número real como base y el exponente (entero positivo) para calcular una potencia.");
-// Pedimos datos al usuario
+    // Inicio del programa, explicación al usuario
+    System.out.println("El programa solicitará un número real como base y el exponente "
+        + "(entero positivo) para calcular una potencia.");
+    // Pedimos datos al usuario
     System.out.print("Introduce la base de la potencia: ");
-    base = s.nextDouble();
+    base = scanner.nextDouble();
     do {
       System.out.print("Introduce el exponente (entero positivo) de la potencia: ");
-      exponente = s.nextInt();
+      exponente = scanner.nextInt();
+      if (exponente <= 0) {
+        System.out.println("Error, el exponente debe ser un entero positivo.");
+      }
     } while (exponente <= 0);
     resultado = base;
-// Realizamos bucle para repetir la multiplicación de la base consigo misma hasta obtener el resultado de la potencia.
-    for (i=1; i<exponente;i+=1){
+    // Realizamos bucle para repetir la multiplicación de la base consigo misma hasta obtener el resultado de la potencia.
+    for (int i=1; i<exponente;i++){
       resultado = (resultado*base);
     }
-// Mostramos el resultado
+    // Mostramos el resultado
     System.out.println("El resultado de la potencia es " + resultado);
   }
 }
--- estructurasRepetitivas/Ej07Intereses.java
+++ estructurasRepetitivas/Ej07Intereses.java
@@ -1,49 +1,38 @@
 package estructurasRepetitivas;
 
-/*
+/**
 07 Ejercicios Estructuras Repetitivas en Java
 
 Programa:
 Una persona adquirió un producto para pagar en 20 meses. El primer mes pagó 10 €,
 el segundo 20 €, el tercero 40 € y así sucesivamente. Realizar un programa para
 determinar cuánto debe pagar mensualmente y el total de lo que pagará después de
-los 20 meses.
+los 20 meses. Este programa puede servir para calcular otras compras y con 
+diferentes plazos.
 
-Autor: Manuel Hidalgo Pérez
+@author: manolohidalgo_
 Fecha: 11-11-19
-
-Variables a utilizar:
-    cuota1 (double)
-    mensualidades (int)
-    totalPagado (double)
-    i (int)
-
-ALGORITMO
-    LEER cuota1, mensualidades
-    Realizar ciclo for realizando (totalPagado = totalPagado *2 ) y en cada ciclo hasta que i = mensualidades
-    MOSTRAR resultado
 */
 
 import java.util.Scanner;
 // Inicio del programa y declaración de variables:
 public class Ej07Intereses {
   public static void main(String[] args){
-    Scanner s = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
     double cuota;
     int mensualidades;
-    int mes;
     double totalPagado = 0;
 
 // Inicio del programa, explicación al usuario
     System.out.println("El programa calcula la cuota correspondiente al pago mensual y el total a pagar al final de la financiación  por la compra de un producto.");
 // Pedimos datos al usuario
     System.out.print("Introduce la primera cuota a pagar: ");
-    cuota = s.nextDouble();
+    cuota = scanner.nextDouble();
     System.out.print("Introduce el número de meses de financiación: ");
-    mensualidades = s.nextInt();
+    mensualidades = scanner.nextInt();
     
-    for (mes=1; mes < mensualidades+1; mes+=1){
-      System.out.println("Cuota "+ mes +": "+ cuota);
+    for (int i=1; i <= mensualidades; i++){
+      System.out.println("Cuota "+ i +": "+ cuota);
       totalPagado = totalPagado + cuota;
       cuota = cuota*2;
     } 

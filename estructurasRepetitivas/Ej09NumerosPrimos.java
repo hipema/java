2669bf591a5ae7bdf924a8e00e7f4d697acf9be7
hipema <a19hipema@iesgrancapitan.org>
2020-09-14 19:27:47 +0200

    Actualizacion ejercicios

--- estructurasRepetitivas/Ej09NumerosPrimos.java
+++ estructurasRepetitivas/Ej09NumerosPrimos.java
@@ -1,59 +1,48 @@
 package estructurasRepetitivas;
 
-/*
+/**
 08 Ejercicios Estructuras Repetitivas en Java
 
 Programa:
 Mostrar en pantalla los N primero número primos. Se pide por teclado la cantidad de números
 primos que queremos mostrar.
 
-Autor: Manuel Hidalgo Pérez
-Fecha: 11-11-19
-
-Variables a utilizar:
-    numPrimos (int)
-    contar (int)
-    primo (int)
-
-ALGORITMO
-    LEER numPrimos
-    Cálculos
-    Mostrar Resultado
+@author: manolohidalgo_
+@date: 11-11-19
 */
 
 import java.util.Scanner;
 // Inicio del programa y declaración de variables:
 public class Ej09NumerosPrimos {
   public static void main(String[] args){
-    Scanner s = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
     int numPrimos;
-    double contar =1;
-    int esPrimo;
+    int contador;
+    boolean esPrimo;
     int divisor;
-    double n = 3;
-    double num;
+    int num;
 
 // Inicio del programa, explicación al usuario
     System.out.println("El programa nos muestra una cantidad solicitada de números primos.");
 // Pedimos datos al usuario
       System.out.print("Indicanos la cantidad de los primeros números primos que deseas conocer: ");
-      numPrimos = s.nextInt();
+      numPrimos = scanner.nextInt();
 // Iniciamos el ciclo para calcular el tiempo.   
     System.out.println("1: 2");
-    contar = 1;
+    contador = 1;
     num = 3;
-    while (contar < numPrimos){
-      esPrimo = 1;
+    while (contador < numPrimos){
+      esPrimo = true;
       divisor=3;
-      while ((divisor<=Math.sqrt((num))) && esPrimo == 1) {
+      while ((divisor<=Math.sqrt((num))) && esPrimo) {
         if (num%divisor==0) {
-          esPrimo = 0;
+          esPrimo = false;
         }
         divisor +=2;
       }
-      if (esPrimo == 1){
-        contar +=1;
-        System.out.println(contar + ": " + num);
+      if (esPrimo){
+        contador +=1;
+        System.out.println(contador + ": " + num);
       }
       num +=2;
     }
